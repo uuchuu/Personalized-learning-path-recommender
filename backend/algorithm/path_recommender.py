9a1662268e4e401824b090c3b1d8e5aa451c060f
uuchuu <dcj186002@163.com>
2026-01-24 01:53:55 +0800

    add learning path recommendation algorithm

--- backend/algorithm/path_recommender.py
+++ backend/algorithm/path_recommender.py
@@ -1,1 +1,32 @@
+from collections import defaultdict, deque
+
+def recommend_learning_path(courses, prerequisites, learned_courses):
+    """
+    courses: dict {course_id: course_name}
+    prerequisites: list of (course_id, pre_course_id)
+    learned_courses: set of course_id
+    """
+
+    graph = defaultdict(list)
+    indegree = defaultdict(int)
+
+    for course, pre in prerequisites:
+        graph[pre].append(course)
+        indegree[course] += 1
+
+    queue = deque()
+    for course in courses:
+        if indegree[course] == 0 and course not in learned_courses:
+            queue.append(course)
+
+    path = []
+    while queue:
+        cur = queue.popleft()
+        path.append(courses[cur])
+        for nxt in graph[cur]:
+            indegree[nxt] -= 1
+            if indegree[nxt] == 0 and nxt not in learned_courses:
+                queue.append(nxt)
+
+    return path
 
